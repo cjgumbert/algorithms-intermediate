45ba727ce9e10695dbd3cc42b8aa7e2037401c73
Corey Joseph Gumbert <cjgumbert@gmail.com>
2017-10-17 16:20:23 -0700

    Create finders-keepers.js

---
+++ finders-keepers.js
@@ -0,0 +1,9 @@
+//Create a function that looks through an array (first argument) and 
+//returns the first element in the array that passes a truth test (second argument).
+
+function findElement(arr, func) {
+  var trueArray = arr.filter(func);
+  return trueArray[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
